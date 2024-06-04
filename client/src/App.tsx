3545f027f3586dc2fc2ff757164075f50f6f6ea5
rizkiromadoni <coradex06@gmail.com>
2024-06-04 18:44:41 +0700

    feat: api logic

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import { useEffect, useState } from "react"
 
 const App = () => {
+  const [input, setInput] = useState<string>("")
+  const [searchResults, setSearchResults] = useState<{
+    results: string[]
+    duration: number
+  }>()
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if(!input) return setSearchResults(undefined)
+
+      const response = await fetch(`/api/search?q=${input}`)
+    }
+
+    fetchData()
+  }, [input])
+
   return (
-    <div>App</div>
+    <div>
+      <input type="text" value={input} onChange={(e) => {
+        setInput(e.target.value)
+      }} />
+    </div>
   )
 }
 
--- server/app.ts
+++ server/app.ts
@@ -1,9 +1,64 @@
+import { Redis } from "@upstash/redis"
 import { Hono } from "hono"
+import { env } from "hono/adapter"
 
 const app = new Hono()
 
-app.get("/api", (c) => {
-    return c.json({ message: "Hello World" })
+type EnvConfig = {
+  UPSTASH_REDIS_REST_TOKEN: string
+  UPSTASH_REDIS_REST_URL: string
+}
+
+app.get("/api/search", async (c) => {
+  try {
+    const { UPSTASH_REDIS_REST_TOKEN, UPSTASH_REDIS_REST_URL } =
+      env<EnvConfig>(c)
+
+    const start = performance.now()
+
+    const redis = new Redis({
+      url: UPSTASH_REDIS_REST_URL,
+      token: UPSTASH_REDIS_REST_TOKEN
+    })
+
+    const query = c.req.query("q")?.toUpperCase()
+
+    if (!query)
+      return c.json(
+        {
+          message: "No query provided"
+        },
+        400
+      )
+
+    const results = []
+    const rank = await redis.zrank("terms", query)
+
+    if (rank !== null && rank !== undefined) {
+      const temp = await redis.zrange<string[]>("terms", rank, rank + 100)
+
+      for (const el of temp) {
+        if (!el.startsWith(query)) break
+
+        if (el.endsWith("*")) {
+          results.push(el.substring(0, el.length - 1))
+        }
+      }
+    }
+
+    const end = performance.now()
+
+    return c.json({
+      results: results,
+      duration: end - start
+    })
+  } catch (error) {
+    console.error(error)
+    return c.json({
+        results: [],
+        message: "Something went wrong"
+    }, 500)
+  }
 })
 
 export default app
